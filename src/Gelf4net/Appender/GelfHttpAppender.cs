d3bb3cc72b066972ac46a14bdfedf98909868892
Leonid Morgenshtern <sleom@hotmail.com>
2016-10-20 14:27:29 +0300

    Add timeout setting for gelf request

--- src/Gelf4net/Appender/GelfHttpAppender.cs
+++ src/Gelf4net/Appender/GelfHttpAppender.cs
@@ -3,6 +3,7 @@ using log4net.Core;
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace gelf4net.Appender
@@ -13,11 +14,15 @@ namespace gelf4net.Appender
 
         private Uri _baseUrl;
 
+        private int _timeout;
+
         public string Url { get; set; }
 
         public string User { get; set; }
 
         public string Password { get; set; }
+
+        public int Timeout { get; set; }
 
         public GelfHttpAppender()
         {
@@ -37,23 +42,26 @@ namespace gelf4net.Appender
                 var byteArray = Encoding.ASCII.GetBytes(User + ":" + Password);
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
             }
+
+            _timeout = Timeout == default(int) ? 5 : Timeout;
         }
 
         protected override void Append(LoggingEvent loggingEvent)
         {
+            var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
             Task.Run(async () =>
             {
                 try
                 {
                     var payload = this.RenderLoggingEvent(loggingEvent);
                     var content = new StringContent(payload, System.Text.Encoding.UTF8, "application/json");
-                    await _httpClient.PostAsync(_baseUrl, content);
+                    await _httpClient.PostAsync(_baseUrl, content, tokenSource.Token);
                 }
                 catch (Exception ex)
                 {
                     this.ErrorHandler.Error("Unable to send logging event to remote host " + this.Url, ex);
                 }
-            });
+            }, tokenSource.Token);
         }
     }
 }
